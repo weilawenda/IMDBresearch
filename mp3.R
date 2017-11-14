674b128660f49eb6c8ec1d5912bc8510e171396a
DejiaTang <nora-tang@hotmail.com>
2017-11-13 22:21:20 -0500

    Wrote sql query to get dataframe to be used.

--- mp3.R
+++ mp3.R
@@ -1,1 +1,16 @@
-###
+library(mdsr)
+library(RMySQL)
+db <- dbConnect_scidb(dbname = "imdb")
+
+sql <- "
+SELECT mi.info_type_id, it.info, t.production_year
+FROM movie_info as mi
+LEFT JOIN info_type as it ON mi.info_type_id = it.id
+LEFT JOIN title as t ON mi.id = t.id
+WHERE production_year is not NULL
+AND mi.info_type_id is not NULL; 
+"
+df <- db %>%
+  dbGetQuery(sql)
+
+glimpse(df)
