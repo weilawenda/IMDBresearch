2c5642a269b7997e08d4a7a9a42647d2a3935aef
Zicong <apple@appledeMacBook-Pro.local>
2017-11-18 20:52:36 -0500

    Add annotations and modify themes

--- mp3.R
+++ mp3.R
@@ -3,6 +3,7 @@
 library(mdsr)
 library(RMySQL)
 library(tidyverse)
+library(ggthemes)
 
 db <- dbConnect_scidb(dbname = "imdb")
 
@@ -54,9 +55,16 @@ glimpse(df1)
 plot1 <- df1 %>%
   ggplot(aes(production_year, value)) +
   geom_line() +
-  facet_wrap( ~ value_type, scales = "free")
+  facet_wrap(~factor(value_type,
+                     labels = c("Average Number of Genres Per Film", "Total Number of Genres")), scale = "free")
 
+# genre_avg , genre_total
+plot1 <- plot1 +
+  theme_fivethirtyeight() +
+  theme(panel.grid.major = element_blank(), axis.title.x=element_blank(), axis.title.y=element_blank())
+  
 plot1
+
 
 df2_total <- df %>%
   group_by(production_year) %>%
@@ -70,6 +78,35 @@ df2 <- df %>%
 plot2 <- df2 %>%
   filter(info == 'Short' | info == 'Drama'| info == 'Comedy') %>%
   ggplot(aes(production_year, proportion, col = info)) +
-  geom_line()
+  geom_line() +
+  ggtitle("Proportion of Short, Drama, and Comedy in Film Genres") +
+  theme_fivethirtyeight() +
+  theme(panel.grid.major = element_blank(),
+        axis.title.x=element_blank(), axis.title.y=element_blank()) +
+  geom_segment(aes(x = 1947, y =0, xend = 1947, yend = 0.5), 
+               color = "black", size =0.1) +
+  geom_text(aes(x = 1947, y = 0.55, label = "United States v.\nParamount Pictures, Inc. 1947"), 
+            size = 3, color = "black") +
+  geom_segment(aes(x = 1994, y =0, xend = 1994, yend = 0.3), 
+               color = "black", size =0.1) +
+  geom_text(aes(x = 1994, y = 0.35, label = "Independent\nFilms Rise 1994"), 
+            size = 3, color = "black") +
+  geom_segment(aes(x = 2000, y =0, xend = 2000, yend = 0.43), 
+               color = "black", size =0.1) +
+  geom_text(aes(x = 2000, y = 0.46, label = "Digital Camera 2000"), 
+            size = 3, color = "black") +
+  geom_segment(aes(x = 2009, y =0, xend = 2009, yend = 0.55), 
+               color = "black", size =0.1) +
+  geom_text(aes(x = 2009, y = 0.58, label = "3D Avatar 2009"), 
+            size = 3, color = "black") +
+  geom_segment(aes(x = 1976, y =0, xend = 1976, yend = 0.35), 
+               color = "black", size =0.1) +
+  geom_text(aes(x = 1976, y = 0.4, label = "Star Wars\n'blockbuster' 1976"), 
+            size = 3, color = "black") +
+  geom_segment(aes(x = 1960, y =0, xend = 1960, yend = 0.35), 
+               color = "black", size =0.1) +
+  geom_text(aes(x = 1960, y = 0.38, label = "Hitchcock 1960"), 
+            size = 3, color = "black")
 
 plot2
+
